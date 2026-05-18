5ebeff9764a1213e59ddec117f724dc9b4bbaba0
pnthai285 <pnthai0000@gmail.com>
2026-05-18 21:57:15 +0000

    test

--- product/src/main/java/com/yas/product/config/DatabaseAutoConfig.java
+++ product/src/main/java/com/yas/product/config/DatabaseAutoConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 @EntityScan({"com.yas.product.model", "com.yas.product.model.attribute"})
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class DatabaseAutoConfig {
+    
     @Bean
     public AuditorAware<String> auditorAware() {
         return () -> {
